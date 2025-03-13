bc00fcf6d1e6d5c5a0e2eb03bbd077fd1b9a8d2d
Juan <j.venegasfuenzalida@gmail.com>
2025-03-13 17:15:44 -0300

    Agregar lógica de visibilidad y animación al spinner personalizado; mejorar la experiencia de carga

--- src/components/Loaders/Loader1.jsx
+++ src/components/Loaders/Loader1.jsx
@@ -1,18 +1,37 @@
-// components/SpinnerPersonalizado.jsx
-import React from "react";
 import logo from "../../assets/MatsushimaChile-Logo.png";
+import React, { useEffect, useState } from 'react';
 
-const SpinnerPersonalizado = () => {
+const SpinnerPersonalizado = ({ onComplete }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isFadingOut, setIsFadingOut] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 100);
+    setTimeout(() => {
+      setIsFadingOut(true);
+      setTimeout(() => {
+        if (onComplete) onComplete();
+      }, 500); // Tiempo de la animación de desaparición
+    }, 2000); // Duración del spinner visible antes de desaparecer
+  }, [onComplete]);
+
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-90 z-50">
+    <div
+      className={`fixed inset-0 flex justify-center items-center bg-white bg-opacity-90 z-50 transition-opacity duration-500 ${
+        isFadingOut ? 'opacity-0' : 'opacity-100'
+      }`}
+    >
       <div className="absolute inset-0 bg-white bg-opacity-100"></div>
-      <div className="absolute inset-0 flex justify-center items-center">
-      <img
-        src={logo}
-        alt="IKO Matsushima Chile"
-        className="w-auto h-auto object-cover"
-      />
-        
+      <div
+        className={`transform transition-transform duration-500 ease-out ${
+          isVisible ? 'scale-100' : 'scale-0'
+        }`}
+      >
+        <img
+          src={logo}
+          alt="IKO Matsushima"
+          className="w-full h-auto transform transition-transform duration-500 ease-in-out"
+        />
       </div>
     </div>
   );
